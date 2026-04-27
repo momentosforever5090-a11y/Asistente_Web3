c44c204239566c0fb998ce5d7268e6f3366ca2b0
momentosforever5090-a11y <momentosforever5090@gmail.com>
2026-04-27 10:15:59 -0400

    Create forms.py

---
+++ forms.py
@@ -0,0 +1,15 @@
+# forms.py
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email, Length
+
+class LoginForm(FlaskForm):
+    username = StringField('Usuario', validators=[
+        DataRequired(message='El usuario es obligatorio'),
+        Length(min=3, max=20, message='Mínimo 3 caracteres')
+    ])
+    password = PasswordField('Contraseña', validators=[
+        DataRequired(message='La contraseña es obligatoria')
+    ])
+    remember = BooleanField('Recordarme')
+    submit = SubmitField('Iniciar sesión')
